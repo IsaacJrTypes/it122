3f796cdbb9130bbb1610fc4804bf4cd0d8c9616f
Isaac J <duckhunterjr@gmail.com>
2022-04-19 21:11:32 -0700

    add minor sytax

--- app.js
+++ app.js
@@ -5,48 +5,48 @@ import fs from 'fs';
 //setup port
 const port = 3000;
 //import data.js
-import * as data from './data.js'
+import * as data from './data.js';
 //import parser
-import { parse } from "querystring";
+import { parse } from 'querystring';
 
 //create pathRedirect
 const pathRedirect = (req, res) => {
-    console.log(req.url);
-    const path = req.url.toLowerCase();
-    const url = path.split("?"); // url split at ? into array
-    let queryObj = parse(url[1]); // turn arrayItem[1] into object
-    let queryDetail = '/detail?' + url[1]; //concat query for routing
+	console.log(req.url);
+	const path = req.url.toLowerCase();
+	const url = path.split('?'); // url split at ? into array
+	let queryObj = parse(url[1]); // turn arrayItem[1] into object
+	let queryDetail = '/detail?' + url[1]; //concat query for routing
 
-    switch (path) {
-        case '/':
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(JSON.stringify(data.getAll()));
-            break;
-        case '/about':
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write('I like turtles');
-            break;
-        case queryDetail:
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(JSON.stringify(data.getItem(data.employees, queryObj.name)));
-            break;
-        default:
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
-            res.end('404: Please try again');
-            break;
-    }
-}
+	switch (path) {
+		case '/':
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.write(JSON.stringify(data.getAll()));
+			break;
+		case '/about':
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.write('I like turtles');
+			break;
+		case queryDetail:
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.write(JSON.stringify(data.getItem(data.employees, queryObj.name)));
+			break;
+		default:
+			res.writeHead(404, { 'Content-Type': 'text/plain' });
+			res.end('404: Please try again');
+			break;
+	}
+};
 
 //create server funct in var
 const server = http.createServer(pathRedirect);
 
 //show err msg, else print port
 const errMsg = (err) => {
-    if (err) {
-        console.log(`There is an error: ${err}`);
-    } else {
-        console.log(`Server is listening on port: ${port}`);
-    }
-}
+	if (err) {
+		console.log(`There is an error: ${err}`);
+	} else {
+		console.log(`Server is listening on port: ${port}`);
+	}
+};
 
 server.listen(port, errMsg);
--- data.js
+++ data.js
@@ -1,24 +1,24 @@
 //create 5 objects
 const employees = [
-    { name: 'pj', position: 'bartender', startTime: '16:00', endTime: '02:00' },
-    { name: 'joe', position: 'bartender', startTime: '16:00', endTime: '02:00' },
-    { name: 'lauren', position: 'server', startTime: '16:00', endTime: '00:00' },
-    { name: 'racheal', position: 'server', startTime: '16:00', endTime: '11:00' },
-    { name: 'fran', position: 'server', startTime: '16:00', endTime: '10:00' }
-]
+	{ name: 'pj', position: 'bartender', startTime: '16:00', endTime: '02:00' },
+	{ name: 'joe', position: 'bartender', startTime: '16:00', endTime: '02:00' },
+	{ name: 'lauren', position: 'server', startTime: '16:00', endTime: '00:00' },
+	{ name: 'racheal', position: 'server', startTime: '16:00', endTime: '11:00' },
+	{ name: 'fran', position: 'server', startTime: '16:00', endTime: '10:00' },
+];
 
 //create getAll method that returns all array items
 const getAll = () => employees;
 
 //create a getItem method that returns a single item
 const getItem = (employees, search) => {
-    const index = employees.findIndex(employee => employee.name === search.toLowerCase())
-    if (index === -1) {
-        return { error: 'No employee with that name' }
-    } else {
-        return employees[index]
-    }//close else 
-}//close getItem
+	const index = employees.findIndex((employee) => employee.name === search.toLowerCase());
+	if (index === -1) {
+		return { error: 'No employee with that name' };
+	} else {
+		return employees[index];
+	} //close else
+}; //close getItem
 
 //export object and methods
-export { employees, getAll, getItem }
+export { employees, getAll, getItem };
